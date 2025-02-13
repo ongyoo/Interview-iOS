c00795a1a4a6e75bd68654b9c18b0efb42ae9c3e
Komsit <ongyoo53@gmail.com>
2025-02-13 12:28:12 +0700

    add xcode project

---
+++ ttb-Interview/ttb-InterviewTests/ttb_InterviewTests.swift
@@ -0,0 +1,17 @@
+//
+//  ttb_InterviewTests.swift
+//  ttb-InterviewTests
+//
+//  Created by "" on 13/2/2568 BE.
+//
+
+import Testing
+@testable import ttb_Interview
+
+struct ttb_InterviewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
